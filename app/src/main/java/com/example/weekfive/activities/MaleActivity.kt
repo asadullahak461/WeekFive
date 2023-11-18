6644e847f45f3a1b1f2a03ed0bf1ff7803efe685
asadullahak461 <mrkhan.ak461@gmail.com>
2023-11-18 22:54:32 +0500

    ui done

---
+++ app/src/main/java/com/example/weekfive/activities/MaleActivity.kt
@@ -0,0 +1,12 @@
+package com.example.weekfive.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.weekfive.R
+
+class MaleActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_male)
+    }
+}
---
+++ app/src/main/java/com/example/weekfive/items.kt
@@ -0,0 +1,4 @@
+package com.example.weekfive
+
+class items {
+}
